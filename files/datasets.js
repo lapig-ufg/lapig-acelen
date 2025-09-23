20d5ec25041da3c40a8271763b8afe266c022e91
boliveirageo <so_geoprocessamento@yahoo.com.br>
2025-09-23 18:01:27 -0300

    Update datasets.js
    Update version 2

--- files/datasets.js
+++ files/datasets.js
@@ -1,7 +1,10 @@
 //Base de dados utilizadas no app
-//Vigor das pastagens
+//Dados
 exports.Dataset = {
   'Vigor-Mapbiomas':ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_pasture_vigor_v1'),
+  'Global Pasture Watch':ee.ImageCollection("projects/global-pasture-watch/assets/ggc-30m/v1/grassland_c"),
+  'Mapbiomas':ee.Image('projects/ee-amazonas21/assets/datasets-app/us-brazil'),
+  'Embeddings':ee.ImageCollection("GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL")
 }
 
 //Imagens das classes agrupadas
