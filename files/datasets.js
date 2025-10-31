f43091cabc9166cb00b0cb33909317581a89f86d
boliveirageo <so_geoprocessamento@yahoo.com.br>
2025-10-31 15:04:28 -0300

    Update datasets.js
    Update version 03

--- files/datasets.js
+++ files/datasets.js
@@ -1,10 +1,24 @@
 //Base de dados utilizadas no app
 //Dados
 exports.Dataset = {
+  //Dataset versão 1.0 toolkit
   'Vigor-Mapbiomas':ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_pasture_vigor_v1'),
-  'Global Pasture Watch':ee.ImageCollection("projects/global-pasture-watch/assets/ggc-30m/v1/grassland_c"),
   'Mapbiomas':ee.Image('projects/ee-amazonas21/assets/datasets-app/us-brazil'),
-  'Embeddings':ee.ImageCollection("GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL")
+  
+  //Dataset versão 2.0 toolkit
+  'Embeddings':ee.ImageCollection("GOOGLE/SATELLITE_EMBEDDING/V1/ANNUAL"),
+  'Global Pasture Watch':ee.ImageCollection("projects/global-pasture-watch/assets/ggc-30m/v1/grassland_c"),
+  
+  //Dataset versão 3.0 toolkit - Dados das bacias hidrográficas
+  'Bacias Hidrográficas':{
+      'DHN250 Nível 1':'projects/mapbiomas-territories/assets/TERRITORIES-OLD/LULC/BRAZIL/COLLECTION9/WORKSPACE/DHN250_LEVEL_1',
+      'DHN250 Nível 2':'projects/mapbiomas-territories/assets/TERRITORIES-OLD/LULC/BRAZIL/COLLECTION9/WORKSPACE/DHN250_LEVEL_2',
+      'DHN250 Nível 3':'projects/mapbiomas-territories/assets/TERRITORIES-OLD/LULC/BRAZIL/COLLECTION9/WORKSPACE/DHN250_LEVEL_3',
+
+      // ------------BACIAS HIDROGRAFICAS DNAEE ------------- //
+      'DNAEE Nível 1':'projects/mapbiomas-territories/assets/TERRITORIES-OLD/LULC/BRAZIL/COLLECTION9/WORKSPACE/BASIN_LEVEL_1_DNAEE',
+      'DNAEE Nível 2':'projects/mapbiomas-territories/assets/TERRITORIES-OLD/LULC/BRAZIL/COLLECTION9/WORKSPACE/BASIN_LEVEL_2_DNAEE',
+  }
 }
 
 //Imagens das classes agrupadas
